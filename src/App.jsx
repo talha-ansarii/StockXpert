631f93a7382a2d615fc2b9d6a976477a469756da
Talha Ansari <talhaansari1606@gmail.com>
2024-10-05 22:22:02 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import HomePage from './pages/Homepage';
 import LoginPage from './pages/LoginPage';
 import ReportPage from './pages/ReportPage';
